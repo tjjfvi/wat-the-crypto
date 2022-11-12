3f18a73bc3c3f890816546ec3c8aadcb30458d8c
tjjfvi <t6@t6.fyi>
2022-11-12 10:03:11 -0800

    add workflows

--- _tasks/build.ts
+++ _tasks/build.ts
@@ -1,12 +1,23 @@
+import * as flags from "https://deno.land/std@0.163.0/flags/mod.ts"
 import * as path from "https://deno.land/std@0.163.0/path/mod.ts"
+import { assertEquals } from "https://deno.land/std@0.163.0/testing/asserts.ts"
 import { encodeHex } from "../util.ts"
+
+const { check } = flags.parse(Deno.args, { boolean: ["check"] })
 
 const wasmPaths = [
   "hashers/xxhash",
   "hashers/blake2b",
 ]
 
-await Promise.all(wasmPaths.map(build))
+let success = true
+await Promise.all(wasmPaths.map((path) =>
+  build(path).catch((error) => {
+    console.error(`${path}:`, error)
+    success = false
+  })
+))
+Deno.exit(success ? 0 : 1)
 
 async function build(wasmPath: string) {
   const process = Deno.run({
@@ -22,14 +33,19 @@ async function build(wasmPath: string) {
 
   const wasm = await process.output()
 
-  await Deno.writeTextFile(
-    wasmPath + ".wasm.ts",
-    `
+  const content = `
 // @generated
 
 import { decodeHex } from "${path.relative(path.dirname(wasmPath), "util.ts")}"
 
 export default decodeHex(\n"${encodeHex(wasm).replace(/.{0,64}|$/g, "\\\n$&")}",\n)
-`.trimStart(),
-  )
+`.trimStart()
+
+  const outputPath = wasmPath + ".wasm.ts"
+  if (check) {
+    const existing = await Deno.readTextFile(outputPath)
+    assertEquals(existing, content, "Outdated file")
+  } else {
+    await Deno.writeTextFile(outputPath, content)
+  }
 }
