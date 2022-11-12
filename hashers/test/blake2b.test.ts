3e8f8760682a8b8b1573c218724cce06f59796c3
tjjfvi <t6@t6.fyi>
2022-11-12 09:17:13 -0800

    update tests

--- hashers/test/blake2b.test.ts
+++ hashers/test/blake2b.test.ts
@@ -1,53 +1,11 @@
-import { assertEquals } from "https://deno.land/std@0.163.0/testing/asserts.ts"
 import * as refImpl from "https://esm.sh/@noble/hashes@1.1.2/blake2b"
-import { encodeHex } from "../../util.ts"
 import { Blake2b } from "../blake2b.ts"
+import { testHasher } from "./test_util.ts"
 
-const lorem =
-  // cspell:disable-next-line
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
-const hashes: [name: string, data: Uint8Array, hash: string][] = [
-  [
-    "empty",
-    new Uint8Array(),
-    "786a02f742015903c6c6fd852552d272912f4740e15847618a86e217f71f5419d25e1031afee585313896444934eb04b903a685b1448b755d56f701afe9be2ce",
-  ],
-  [
-    "[0; 1024]",
-    new Uint8Array(1024),
-    "b4b72b45c308e963f4c927827426228c0ed898403411ad108fbd0611e146ccd494bda4b9a593c33d7cf49931748e8bc29a829d50e904305cb38dfb1443532ef7",
-  ],
-  [
-    "0..256",
-    new Uint8Array(Array.from({ length: 256 }, (_, i) => i)),
-    "1ecc896f34d3f9cac484c73f75f6a5fb58ee6784be41b35f46067b9c65c63a6794d3d744112c653f73dd7deb6666204c5a9bfa5b46081fc10fdbe7884fa5cbf8",
-  ],
-  [
-    "lorem",
-    new TextEncoder().encode(lorem),
-    "bf6a2cf4132a6436f90190ebbf436dd622a73294d44348046c32504c224aaa3d1c80ca83107eb9548ccba36e141183b3bfb0f38779c0d9ab76b5971590e79d5e",
-  ],
-]
-
-for (const [name, data, hash] of hashes) {
-  Deno.test(`${name} reference`, () => {
-    assertEquals(encodeHex(refImpl.blake2b(data)), hash)
+for (const size of [512, 8, 16, 32, 64, 128, 256] as const) {
+  testHasher({
+    name: `blake2b ${size}`,
+    reference: (data) => refImpl.blake2b(data, { dkLen: size / 8 }),
+    create: () => new Blake2b(size / 8),
   })
-  Deno.test(`${name} straight`, () => {
-    const hasher = new Blake2b()
-    hasher.update(data)
-    assertEquals(encodeHex(hasher.digest()), hash)
-    hasher.dispose()
-  })
-  for (const chunkSize of [1, 13, 31, 32, 33, 49, 64, 65, 113]) {
-    Deno.test(`${name} chunked ${chunkSize}`, () => {
-      const hasher = new Blake2b()
-      for (let i = 0; i < data.length; i += chunkSize) {
-        hasher.update(data.slice(i, i + chunkSize))
-      }
-      assertEquals(encodeHex(hasher.digest()), hash)
-      hasher.dispose()
-    })
-  }
 }
---
+++ hashers/test/test_util.ts
@@ -0,0 +1,53 @@
+import { assertEquals } from "https://deno.land/std@0.163.0/testing/asserts.ts"
+import { assertSnapshot } from "https://deno.land/std@0.163.0/testing/snapshot.ts"
+import { encodeHex } from "../../util.ts"
+import { Hasher } from "../common.ts"
+
+interface TestHasherProps {
+  name: string
+  reference: (data: Uint8Array) => Uint8Array
+  create: () => Hasher
+}
+
+const lorem =
+  // cspell:disable-next-line
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+const license = await fetch(new URL("../../LICENSE", import.meta.url)).then((r) => r.arrayBuffer())
+
+const testCases: [name: string, data: Uint8Array][] = [
+  ["empty", new Uint8Array()],
+  ["[0; 1024]", new Uint8Array(1024)],
+  ["0..256", new Uint8Array(Array.from({ length: 256 }, (_, i) => i))],
+  ["lorem", new TextEncoder().encode(lorem)],
+  ["license", new Uint8Array(license)],
+]
+
+export function testHasher(props: TestHasherProps) {
+  Deno.test(props.name, async (t) => {
+    for (const [name, data] of testCases) {
+      await t.step(name, async (t) => {
+        const hash = encodeHex(props.reference(data))
+        await t.step(`reference`, async (t) => {
+          await assertSnapshot(t, hash)
+        })
+        await t.step(`straight`, () => {
+          const hasher = props.create()
+          hasher.update(data)
+          assertEquals(encodeHex(hasher.digest()), hash)
+          hasher.dispose()
+        })
+        for (const chunkSize of [1, 13, 31, 32, 33, 49, 64, 65, 113]) {
+          await t.step(`chunked ${chunkSize}`, () => {
+            const hasher = props.create()
+            for (let i = 0; i < data.length; i += chunkSize) {
+              hasher.update(data.subarray(i, i + chunkSize))
+            }
+            assertEquals(encodeHex(hasher.digest()), hash)
+            hasher.dispose()
+          })
+        }
+      })
+    }
+  })
+}
