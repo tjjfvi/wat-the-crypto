2914b78203ff4b0faa9a131499f0520e0850bd5c
tjjfvi <t6@t6.fyi>
2023-05-17 17:34:21 -0400

    ...

--- sr25519/sr25519.ts
+++ sr25519/sr25519.ts
@@ -18,14 +18,16 @@ const wasmInstance = new WebAssembly.Instance(wasmModule, {
 })
 
 interface Sr25519Wasm {
+  keccak_rc_adr: WebAssembly.Global
   coef: WebAssembly.Global
   exp: WebAssembly.Global
   neg_u256_mod_exp: WebAssembly.Global
   neg_coef: WebAssembly.Global
   neg_exp: WebAssembly.Global
-
   free_adr: WebAssembly.Global
+
   keccak_f1600(adr: number): void
+
   _u256_add(o: number, s: bigint, x: number, n: bigint): number
   u256_sub(o: number, x: number, y: number): number
   _u256_mul_u512(o: number, x: number, y: number): void
@@ -36,6 +38,38 @@ interface Sr25519Wasm {
 
 export const wasm = wasmInstance.exports as never as Sr25519Wasm
 export const mem = new Uint8Array(memory.buffer)
+
+mem.set(
+  new Uint8Array(
+    new BigUint64Array([
+      0x0000000000000001n,
+      0x0000000000008082n,
+      0x800000000000808an,
+      0x8000000080008000n,
+      0x000000000000808bn,
+      0x0000000080000001n,
+      0x8000000080008081n,
+      0x8000000000008009n,
+      0x000000000000008an,
+      0x0000000000000088n,
+      0x0000000080008009n,
+      0x000000008000000an,
+      0x000000008000808bn,
+      0x800000000000008bn,
+      0x8000000000008089n,
+      0x8000000000008003n,
+      0x8000000000008002n,
+      0x8000000000000080n,
+      0x000000000000800an,
+      0x800000008000000an,
+      0x8000000080008081n,
+      0x8000000000008080n,
+      0x0000000080000001n,
+      0x8000000080008008n,
+    ]).buffer,
+  ),
+  wasm.keccak_rc_adr.value,
+)
 
 writeU256(wasm.coef.value, coef)
 writeU256(wasm.exp.value, exp)
